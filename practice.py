d9b37031b77668a35a4cdf272ed1ac5f5dc47f3b
KrishnaTanna2808 <kp999003@gmail.com>
2024-06-25 16:57:00 +0530

    celsius

--- practice.py
+++ practice.py
@@ -1,10 +1,54 @@
-name = (input("enter the name:"))
+#name = (input("enter the name:"))
 
-print(name)
-print(type(name))
+#print(name)
+#print(type(name))
 
-num1 = int(input("enter first number:"))
-num2 = int(input("enter second number:"))
-x = [num1+num2]
-print("total marks:" +str(x))
+#num1 = int(input("enter first number:"))
+#num2 = int(input("enter second number:"))
+#x = [num1+num2]
+#print("total marks:" +str(x))
+
+#num1 = int(input("enter the first number: "))
+
+#num2 = int(input("enter the second number: "))
+
+
+#print("addition:" + str(num1 + num2))
+#print("substraction:" + str(num1 - num2))
+#print("multiplication:" + str(num1 * num2))
+#print("division:" + str(num1 / num2))
+#write the program of finding the perimeter
+#n1 = int(input("length of rectangle: "))
+#n2 = int(input("breadth of the rectangle: "))
+
+#n3 = 2*(n1 + n2)
+#print("perimeter of rectangle:" + str(n3))
+
+#to find its area
+#n3 = n1 * n2
+#print("area of reactangle:" + str(n3))
+
+#calculate the radius,diameter,circumference and area
+
+#circle = int(input("enter the number: "))
+
+#print("radius: " + str(circle/2))
+#print("diameter: " + str(2*circle))
+
+convert = int(input("enter the lenght in centimeter: "))
+
+print("in meters: " + str(convert/100))
+print("in kilometers: " + str(convert/10000))
+
+
+num = float(input("enter into celsius: "))
+h = (9/5)*num + 32
+print("in fahrenheit:" + str(h))
+
+g = num+32*(5/9)
+fahrenheit = float(input("enter into fahrenheit:"))
+
+print("in celsius: " + str(g))
+
+ 
 
